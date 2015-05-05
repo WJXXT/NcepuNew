af31d071afac9c18024db611571f8bfae9d7282c
王新国 <wangxinguo@wangxinguodeMacBook-Air.local>
2015-05-05 09:01:22 +0800

    Initial Commit

---
+++ NcepuNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NcepuNews
+//
+//  Created by 王新国 on 15/5/5.
+//  Copyright (c) 2015年 XXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
